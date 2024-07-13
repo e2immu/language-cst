3a5d5cbe4efa8d52a2aa93fbe95fef24547e16bc
Bart Naudts <Bart.Naudts@codelaser.io>
2024-07-13 08:34:57 +0200

    wrong interface: DivideImpl implements Divide

--- e2immu-cst-impl/src/main/java/org/e2immu/language/cst/impl/expression/DivideImpl.java
+++ e2immu-cst-impl/src/main/java/org/e2immu/language/cst/impl/expression/DivideImpl.java
@@ -2,16 +2,16 @@ package org.e2immu.language.cst.impl.expression;
 
 import org.e2immu.language.cst.api.element.Comment;
 import org.e2immu.language.cst.api.element.Source;
+import org.e2immu.language.cst.api.expression.Divide;
 import org.e2immu.language.cst.api.expression.Expression;
 import org.e2immu.language.cst.api.expression.Precedence;
-import org.e2immu.language.cst.api.expression.Product;
 import org.e2immu.language.cst.api.info.MethodInfo;
 import org.e2immu.language.cst.api.runtime.Runtime;
 import org.e2immu.language.cst.api.type.ParameterizedType;
 
 import java.util.List;
 
-public class DivideImpl extends BinaryOperatorImpl implements Product {
+public class DivideImpl extends BinaryOperatorImpl implements Divide {
 
     public DivideImpl(List<Comment> comments, Source source, MethodInfo operator, Precedence precedence,
                       Expression lhs, Expression rhs, ParameterizedType parameterizedType) {
