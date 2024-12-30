05b74c77748e15f7b616b0c8ee296e9e55418615
Bart Naudts <Bart.Naudts@codelaser.io>
2024-12-30 08:07:28 +0100

    small extension of EvalDivide: a/n/m = a/(n*m)

--- e2immu-cst-impl/src/main/java/org/e2immu/language/cst/impl/expression/eval/EvalDivide.java
+++ e2immu-cst-impl/src/main/java/org/e2immu/language/cst/impl/expression/eval/EvalDivide.java
@@ -1,10 +1,12 @@
 package org.e2immu.language.cst.impl.expression.eval;
 
+import org.e2immu.language.cst.api.expression.Divide;
 import org.e2immu.language.cst.api.expression.Expression;
 import org.e2immu.language.cst.api.expression.IntConstant;
 import org.e2immu.language.cst.api.expression.Numeric;
 import org.e2immu.language.cst.api.runtime.Runtime;
 import org.e2immu.language.cst.impl.expression.DivideImpl;
+import org.e2immu.language.cst.impl.expression.IntConstantImpl;
 
 public class EvalDivide {
     private final Runtime runtime;
@@ -20,6 +22,10 @@ public class EvalDivide {
         if (lhs instanceof IntConstant li && rhs instanceof IntConstant ri && ri.constant() != 0)
             return runtime.newInt(li.constant() / ri.constant());
 
+        // a/n1/n2 = a/n1*n2
+        if (lhs instanceof Divide d2) {
+            return new DivideImpl(runtime, d2.lhs(), runtime.product(d2.rhs(), rhs));
+        }
         // any unknown lingering
         if (lhs.isEmpty() || rhs.isEmpty()) throw new UnsupportedOperationException();
 
