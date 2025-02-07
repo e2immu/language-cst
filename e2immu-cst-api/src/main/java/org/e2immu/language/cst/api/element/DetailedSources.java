6d18fe7b74ce58719f14b53df7cfdf6c00780da4
Bart Naudts <Bart.Naudts@codelaser.io>
2025-02-07 11:34:57 +0100

    added marker object in DetailedSources

--- e2immu-cst-api/src/main/java/org/e2immu/language/cst/api/element/DetailedSources.java
+++ e2immu-cst-api/src/main/java/org/e2immu/language/cst/api/element/DetailedSources.java
@@ -3,6 +3,10 @@ package org.e2immu.language.cst.api.element;
 import java.util.List;
 
 public interface DetailedSources {
+    // used to grab the closing parenthesis of the record field list,
+    // the closing parenthesis of any method declaration's form parameter list
+    Object END_OF_PARAMETER_LIST = new Object();
+
     Source detail(Object object);
 
     // use for types
