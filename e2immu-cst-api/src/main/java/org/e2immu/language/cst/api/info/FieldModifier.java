dd1e2e434894f9e19dc57bd58f1dd498ab09de50
Bart Naudts <Bart.Naudts@codelaser.io>
2025-03-24 12:54:57 +0100

    convenience methods in FieldModifier, MethodModifier

--- e2immu-cst-api/src/main/java/org/e2immu/language/cst/api/info/FieldModifier.java
+++ e2immu-cst-api/src/main/java/org/e2immu/language/cst/api/info/FieldModifier.java
@@ -3,6 +3,10 @@ package org.e2immu.language.cst.api.info;
 import org.e2immu.language.cst.api.output.element.Keyword;
 
 public interface FieldModifier {
+    default boolean isAccessModifier() {
+        return isPublic() || isPrivate() || isProtected();
+    }
+
     boolean isStatic();
 
     boolean isFinal();
--- e2immu-cst-api/src/main/java/org/e2immu/language/cst/api/info/MethodModifier.java
+++ e2immu-cst-api/src/main/java/org/e2immu/language/cst/api/info/MethodModifier.java
@@ -5,6 +5,10 @@ import org.e2immu.language.cst.api.output.element.Keyword;
 
 public interface MethodModifier {
 
+   default boolean isAccessModifier() {
+       return isPublic() || isPrivate() || isProtected();
+   }
+
     boolean isPublic();
 
     boolean isPrivate();
