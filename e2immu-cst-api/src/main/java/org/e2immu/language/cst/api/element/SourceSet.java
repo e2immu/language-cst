d339ba8d9cdecc511318c347731391c9c1382c31
Bart Naudts <Bart.Naudts@codelaser.io>
2025-04-29 14:35:21 +0200

    2nd iteration of SourceSet, removed ClassPathPart

--- e2immu-cst-api/src/main/java/org/e2immu/language/cst/api/element/SourceSet.java
+++ e2immu-cst-api/src/main/java/org/e2immu/language/cst/api/element/SourceSet.java
@@ -1,12 +1,20 @@
 package org.e2immu.language.cst.api.element;
 
+import java.net.URI;
 import java.nio.charset.Charset;
 import java.nio.file.Path;
 import java.util.Set;
 
+/**
+ * Possible situations.
+ * <p>
+ * There are quite a few external libraries that contain java sources in the .jar file.
+ * If an external library has no sources, they can be generated using a decompiler. Either way, they should end up
+ * in the sourceDirectory() for the modification analyzer to be able to produce Annotated-API files.
+ */
 public interface SourceSet {
 
-    Charset encoding();
+    Charset sourceEncoding();
 
     String name();
 
@@ -16,13 +24,40 @@ public interface SourceSet {
      * <p>
      * If this source set represents an external library, this path is either <code>null</code>, or points to
      * the directory where sources have been expanded or computed using a decompiler.
-     * The location of the library (jar) will be part of extensions of this object (<code>ClassPathPart</code>).
+     * The location of the library (jar) is given by the <code>uri()</code>.
      * <p>
+     *
      * @return a path representing a directory containing source files
      */
-    Path path();
+    Path sourceDirectory();
+
+    /**
+     * Valid URI with a non-null scheme.
+     * <ul>
+     *     <li>source directory: 'file' scheme, logically identical to <code>sourceDirectory()</code></li>
+     *     <li>external jar on file system: a file with the 'file' scheme, name ends in .jar</li>
+     *     <li>a test-protocol entry in a fqn->source map: 'test-protocol' scheme </li>
+     * </ul>
+     *
+     * <p>
+     * Some schemes have a special meaning, and will be intercepted: "test-protocol", "jar-on-classpath", "jmod".
+     *
+     * @return this source set's URI. Cannot be null.
+     */
+    URI uri();
+
+    default boolean parsedFromSource() {
+        return !externalLibrary();
+    }
 
     boolean test();
+
+    /**
+     * only relevant when external library is true
+     *
+     * @return if the source set is a library which is needed at runtime, but not at compile time.
+     */
+    boolean runtimeOnly();
 
     boolean library();
 
@@ -63,4 +98,8 @@ public interface SourceSet {
     // helper methods
 
     boolean acceptSource(String packageName, String typeName);
+
+    SourceSet withPath(Path path);
+
+    SourceSet withPathUri(Path path, URI uri);
 }
