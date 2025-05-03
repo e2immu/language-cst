af4c1a2ebc6d13cdb31db57a2f3beeef78b69122
Bart Naudts <Bart.Naudts@codelaser.io>
2025-05-03 08:27:46 +0200

    added noFingerPrint() method

--- e2immu-cst-api/src/main/java/org/e2immu/language/cst/api/element/FingerPrint.java
+++ e2immu-cst-api/src/main/java/org/e2immu/language/cst/api/element/FingerPrint.java
@@ -8,4 +8,7 @@ package org.e2immu.language.cst.api.element;
  * Only relevant methods are <code>Object.equals()</code> and <code>Object.toString()</code>.
  */
 public interface FingerPrint {
+
+    boolean isNoFingerPrint();
+
 }
