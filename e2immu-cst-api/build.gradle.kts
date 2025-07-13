cb3d6cc4ee91a37f73d2051dc2ba5e288ed9e8af
Bart Naudts <bart.naudts@codelaser.io>
2025-07-13 10:28:13 +0200

    java 24

--- e2immu-cst-api/build.gradle.kts
+++ e2immu-cst-api/build.gradle.kts
@@ -23,8 +23,8 @@ repositories {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_21
-    targetCompatibility = JavaVersion.VERSION_21
+    sourceCompatibility = JavaVersion.VERSION_24
+    targetCompatibility = JavaVersion.VERSION_24
 }
 
 dependencies {
